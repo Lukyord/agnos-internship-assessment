9b22a73d183fe5fa933ba1dcec0717398b855597
Lukyord <ttansiri524@gmail.com>
2022-12-07 22:35:07 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="texl-3xl bg-slate-400">Hello World</div>;
+}
+
+export default App;
