8a57583f1ae3e174dbd287c085a533f4299c6609
Lukyord <ttansiri524@gmail.com>
2022-12-07 23:08:03 +0700

    Added navbar features, tab icon and tab title

--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -1,3 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function MainNavigation() {
-  return <div>MainNavigation</div>;
+  return (
+    <div className="flex h-[6rem] w-screen justify-start items-center px-[1rem] border-b-2 shadow-xl">
+      <Link to="/">
+        <h1 className="text-6xl italic font-extrabold px-[1rem]">
+          <text className="text-blue-600">BASIc</text>{" "}
+          <text className="text-cyan-600">DIAGNOSEs</text>
+        </h1>
+      </Link>
+      <p>search bar</p>
+    </div>
+  );
 }
